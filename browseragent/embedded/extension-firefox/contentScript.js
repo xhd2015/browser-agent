98bf9cda0fd43a4ae204e031693d581a92dcab04
xhd2015 <xhd2015@gmail.com>
2026-08-04 09:42:37 +0800

    bump version to 1.0.10

--- browseragent/embedded/extension-firefox/contentScript.js
+++ browseragent/embedded/extension-firefox/contentScript.js
@@ -13,7 +13,7 @@
       product: "browser-agent",
       browser: "firefox",
       controlPort: 43761,
-      version: "1.0.1",
+      version: "1.0.10",
       features: ["browser-agent"],
     };
   } catch (e) {

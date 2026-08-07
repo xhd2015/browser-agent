fb1e789f4bf2b8cf06ce9333060cecad6855d812
xhd2015 <xhd2015@gmail.com>
2026-08-07 23:10:58 +0800

    bump to v1.0.12

--- browseragent/embedded/extension-firefox/bundle-sum.js
+++ browseragent/embedded/extension-firefox/bundle-sum.js
@@ -1,3 +1,3 @@
 // browser-agent bundle-sum — generated; do not edit
-var BROWSER_AGENT_BUNDLE_VERSION = "1.0.11";
-var BROWSER_AGENT_BUNDLE_MD5 = "c223c2e44a8d45bfce575ec06702f357";
+var BROWSER_AGENT_BUNDLE_VERSION = "1.0.12";
+var BROWSER_AGENT_BUNDLE_MD5 = "05a81026842e41fc872bea00c493be9f";
